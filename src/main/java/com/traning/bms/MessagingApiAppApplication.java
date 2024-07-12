db5611b2d0e62bfaedc499ffd17bdd22e19bf912
Akshath G <akshag@dhcp-10-120-130-92.wifi.oracle.com>
2024-07-12 14:21:51 +0530

    first commit

---
+++ src/main/java/com/traning/bms/MessagingApiAppApplication.java
@@ -0,0 +1,20 @@
+package com.traning.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MessagingApiAppApplication {
+	@GetMapping("/")
+	public String printGreetings() {
+		return "Hi Oracle !!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApiAppApplication.class, args);
+		
+	}
+
+}
