a60318fb053aadb13ed1d9ee2dec10ff34e12e75
Akshath G <akshag@dhcp-10-120-130-92.wifi.oracle.com>
2024-07-12 15:07:36 +0530

    HELLO askhath

--- src/main/java/com/traning/bms/MessagingApiAppApplication.java
+++ src/main/java/com/traning/bms/MessagingApiAppApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MessagingApiAppApplication {
-	@GetMapping("/")
+	@GetMapping("/admin")
 	public String printGreetings() {
 		return "Hi Oracle !!";
 	}
